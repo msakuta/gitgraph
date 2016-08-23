e0a639097160bda9e86e11bbb2622b2e6880ff8a
msakuta <masahiro.sakuta@gmail.com>
2016-08-22 20:03:12 -0400

    Recognize merge commit

--- js/gitgraph.js
+++ js/gitgraph.js
@@ -4,11 +4,17 @@ window.gitgraph = new (function(){
 /** Parse raw output from `git log --stat --raw` and format for HTML
  */
 this.parseLog = function(text, commitsElem){
-	var commits = text.match(/^commit [0-9a-f]+/mg)
+	var commits = text.match(/^commit [0-9a-f]+(\nMerge:( [0-9a-f]+)+)?/mg)
 	if(!commits)
 		return
-	for(var i = 0; i < commits.length; i++)
-		commitsElem.innerHTML += commits[i] + '\n'
+	for(var i = 0; i < commits.length; i++){
+		var str = commits[i]
+		var commitStr = str.match(/^commit [0-9a-f]+/)
+		var mergeStr = str.match(/Merge:( [0-9a-f]+)+/)
+		if(mergeStr)
+			commitStr += '  ' + mergeStr[0]
+		commitsElem.innerHTML += commitStr + '\n'
+	}
 }
 
 })()
